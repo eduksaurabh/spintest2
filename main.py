37e53c84b98ac6345bf861b6ad736b6696fb8ce5
eduksaurabh <52511810+eduksaurabh@users.noreply.github.com>
2021-09-20 17:07:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello CICD World'
+    return 'Hello automation World'
 
 @app.errorhandler(500)
 def server_error(e):
